705317f0a2c680503d7947d379c96f2e537093bc
Caleb Kim <calebmk21@gmail.com>
2023-02-28 16:35:31 -0800

    it doesn't work but it's not broken either

---
+++ Assets/HeronBehavior.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeronBehavior : StateMachineBehaviour
+{
+    public float moveSpeed = 1.5f;
+    public float riseRange = 0.1f;
+    Transform player;
+    Rigidbody2D rb;
+
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, moveSpeed * Time.fixedDeltaTime);
+        
+
+        // if (Vector2.Distance(player.position, rb.position) > riseRange)
+        // {
+        //     animator.SetTrigger("Rise");
+        //     rb.MovePosition(newPos);
+        //     Debug.Log("Rising");
+        // }
+         
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
