3fd6dfaf571ab08be5132daaa51099c120aa2283
Caleb Kim <calebmk21@gmail.com>
2023-03-06 22:47:58 -0800

    device swapping - hitboxes almost implemented

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    public Animator animator; 
+
+    public Transform attackPoint;
+    public Transform hitbox;
+
+    public LayerMask pLayer;
+
+    public Rigidbody2D rb;
+
+
     public void DealingDamage(int damage)
     {
         
--- Assets/Scripts/Enemies/Heron Manager/HeronAttacks.cs
+++ Assets/Scripts/Enemies/Heron Manager/HeronAttacks.cs
@@ -6,18 +6,37 @@ public class HeronAttacks : MonoBehaviour
 {
     public int attackDamage = 20;
 	public int enragedAttackDamage = 40;
+	public Transform hitbox;
+	public LayerMask pLayer;
 
 	public Vector3 attackOffset;
 	public float attackRange = 1f;
 	public LayerMask attackMask;
+	Vector2 pos; 
+	Rigidbody2D rb;
 
+	void Start()
+	{
+		rb = GameObject.FindGameObjectWithTag("Heron").GetComponent<Rigidbody2D>(); 
+	}
+
+	void Update()
+	{
+		pos = rb.position;
+	}
 
     public void DiveAttack()
     {
-        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        Collider2D colInfo = Physics2D.OverlapCircleAll(hitbox.position, attackRange, attackMask);
 		if (colInfo != null)
 		{
 			colInfo.GetComponent<Health>().Damage(attackDamage);
+			Debug.Log("Current HP: " + colInfo.GetComponent<Health>().health);
 		}
     }
+
+	void OnDrawGizmosSelected()
+	{
+		if 
+	}
 }
